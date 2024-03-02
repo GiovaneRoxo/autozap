d71f39d572cafcb3c0e427a75f664248588daf63
Marelso <klein.marcelojr@gmail.com>
2024-03-02 15:28:47 -0300

    Including file to handle uninstallation

---
+++ uninstall.js
@@ -0,0 +1,14 @@
+const {resolve} = require('path');
+let path = resolve('./app.js')
+console.log(path);
+
+var Service = require('node-windows').Service;
+var svc = new Service({
+    name: 'autozap',
+    description: 'autozap test service',
+    script: path
+})
+
+svc.on('uninstall',function(){
+    console.log('Uninstall complete.');
+});
