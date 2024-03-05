cd6f512b0fa1c959ec52a3df15d988af9be6df9e
Marelso <klein.marcelojr@gmail.com>
2024-03-05 05:53:38 -0300

    Including file to handle database service interaction

---
+++ services/DatabaseService.js
@@ -0,0 +1,35 @@
+const sqlite3 = require('sqlite3').verbose();
+const Attendant = require('../domain/Attendant');
+
+class DatabaseService {
+    constructor(dbFilePath) {
+        this.db = new sqlite3.Database(dbFilePath);
+    }
+
+    getAllAttendants(callback) {
+        this.db.all("SELECT * FROM attendant", (err, rows) => {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            const attendants = rows.map(row => new Attendant(row.id, row.name, row.bio, row.link));
+            callback(null, attendants);
+        });
+    }
+
+    getNextAttendantById(currentAttendantId, callback) {
+        this.db.get("SELECT * FROM attendant WHERE id = (SELECT COALESCE((SELECT id FROM attendant WHERE id > ? LIMIT 1),(SELECT id FROM attendant ORDER BY id ASC LIMIT 1)))", [currentAttendantId], (err, row) => {
+            if (err) {
+                return;
+            }
+            if (!row) {
+                return;
+            }
+            const attendant = new Attendant(row.id, row.name, row.bio, row.link);
+            callback(null, attendant)
+        });
+    }
+
+}
+
+module.exports = DatabaseService;
