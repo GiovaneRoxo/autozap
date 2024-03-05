c96ef9e7d55bb9a9e67791e3a7728d66af1bc02c
Marelso <klein.marcelojr@gmail.com>
2024-03-05 05:53:18 -0300

    Including file to handle attendant data model

---
+++ domain/Attendant.js
@@ -0,0 +1,10 @@
+class Attendant {
+    constructor(id, name, bio, link) {
+        this.id = id;
+        this.name = name;
+        this.bio = bio;
+        this.link = link;
+    }
+}
+
+module.exports = Attendant;
