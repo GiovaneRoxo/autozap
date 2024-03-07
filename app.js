b60a1dc8e8068e95ae34cfd4c365599e05f4a26c
Giovane Roxo <101137945+GiovaneRoxo@users.noreply.github.com>
2024-03-06 22:07:43 -0300

    Revert "Feature | listen as a service"

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const DatabaseService = require('./services/DatabaseService');
-
-const dbService = new DatabaseService('../org.autozap.dashboard/database.db');
-
-let currentAttendantId = 0;
-let clients = [];
-
-clients.push('555186549768@c.us')
-
-dbService.getAllAttendants((err, attendants) => {
-    if (err) {
-        console.error('Error fetching attendants from the database:', err);
-        return;
-    }
-    console.log('Attendants:', attendants);
-});
+const { Client } = require('whatsapp-web.js');
+const fs = require('fs');
+const qrcode = require('qrcode-terminal');
+const {LocalAuth } = require('whatsapp-web.js');
+const atendentesPath = './atendentes.json';
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        clientId: 'client-one',
-        dataPath: 'auth'
-    })
+    authStrategy: new LocalAuth()
+});
+
+let atendentes = [];
+let atendentesLength;
+let atendenteIndex = 0;
+let clients = [];
+
+fs.readFile(atendentesPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo de atendentes:', err);
+        return;
+    }
+    atendentes = JSON.parse(data);
+    atendentesLength = atendentes.length;
+    console.log(atendentesLength, 'atendentes carregados');
+    console.log('Lista de atendentes carregada:', atendentes);
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
@@ -28,20 +33,20 @@ client.on('ready', () => {
 });
 
 client.on('message_create', async (msg) => {
-    console.log(msg);
-    const sender = msg.from;
+    const sender = msg.from.includes("85067794") ? msg.to : msg.from
     const chat = await msg.getChat();
     const isGroup = chat.isGroup;
-
-    if (!isGroup && !clients.includes(msg.from)){
-        dbService.getNextAttendantById(currentAttendantId, (err, attendant) => { 
-            let message = `Olá! Você será atendido por ${attendant.name}, ${attendant.bio}, para continuar o atendimento, clique no link a seguir: ${attendant.link}`;
-            client.sendMessage(sender, message);
-            clients.push(msg.from);
-            currentAttendantId = attendant.id;
-        })
+    if (msg.body === "oi" && !clients.includes(msg.from) && !isGroup){
+        const atendente = atendentes[atendenteIndex]
+        const message = `Olá! Você será atendido por ${atendente.nome}, ${atendente.bio}, para continuar o atendimento, clique no link a seguir: ${atendente.link}`;
+        client.sendMessage(sender, message);
+        clients.push(msg.from);
+        if(atendenteIndex < atendentesLength - 1) {
+            atendenteIndex++;
+        } else {
+            atendenteIndex = 0;
+        }
     }
 });
 
 client.initialize();
-process.stdin.resume();
