997520f6ef078d90c8b3d09ab1ac7b7759d31175
Marelso <klein.marcelojr@gmail.com>
2024-03-09 22:14:12 -0300

    Including new db method

--- services/DatabaseService.js
+++ services/DatabaseService.js
@@ -17,6 +17,18 @@ class DatabaseService {
         });
     }
 
+    getMessage(callback) {
+        this.db.get("SELECT message FROM message WHERE active = true LIMIT 1", (err, row) => {
+            if (err) {
+                return;
+            }
+            if (!row) {
+                return;
+            }
+            callback(null, row.message)
+        });
+    }
+
     getNextAttendantById(currentAttendantId, callback) {
         this.db.get("SELECT * FROM attendant WHERE id = (SELECT COALESCE((SELECT id FROM attendant WHERE id > ? LIMIT 1),(SELECT id FROM attendant ORDER BY id ASC LIMIT 1)))", [currentAttendantId], (err, row) => {
             if (err) {
