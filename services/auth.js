db2a986226905bae39127689cf67b2d7cb10d202
Marelso <klein.marcelojr@gmail.com>
2024-03-10 12:49:40 -0300

    Translating strings to pt-br

--- services/auth.js
+++ services/auth.js
@@ -11,25 +11,23 @@ const client = new Client({
 let hasAuthenticated = false
 
 client.on('qr', (qr) => {
+    console.log('Gerando QR code...')
     qrcode.generate(qr, { small: true });
     hasAuthenticated = true
 });
 
 client.on('ready', () => {
     if(hasAuthenticated) {
-        console.log('Client is ready!');
+        console.log('Client autenticado com sucesso!');
         client.destroy().then(() => {
-            console.log('Client destroyed successfully.');
+            console.log('Encerrando client.');
             process.exit();
         }).catch((error) => {
             console.error('Error while destroying client:', error);
             process.exit(1);
         });
     } else {
-        console.log('Performing logout');
-        client.logout().then(
-            client.initialize()
-        )
+        console.log('Encontramos um login, desconecte o dispositivo do seu whatsapp.')
     }
 });
 
